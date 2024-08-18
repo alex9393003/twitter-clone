4c526d8bf542520ee96682a0414232bea06ac955
Alex <alexandrosavgoustiss@gmail.com>
2024-08-18 18:21:02 +0300

    Update config.ts

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -4,8 +4,8 @@ const config = {
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-} as const;
+  appId: process.env.NEXT_PUBLIC_APP_ID
+} as const; // Remove the trailing comma here
 
 type Config = typeof config;
 
