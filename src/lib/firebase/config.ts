9ab1186dba194e432ea16608755ea78143110040
Alex <alexandrosavgoustiss@gmail.com>
2024-08-18 18:15:41 +0300

    Update config.ts

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -5,13 +5,4 @@ const firebaseConfig = {
   storageBucket: "lolt-bb9aa.appspot.com",
   messagingSenderId: "632100441282",
   appId: "1:632100441282:web:4dc8d680f755e7d0baff5b"
-} as const;
-
-type Config = typeof config;
-
-export function getFirebaseConfig(): Config {
-  if (Object.values(config).some((value) => !value))
-    throw new Error('Firebase config is not set or incomplete');
-
-  return config;
-}
+};
